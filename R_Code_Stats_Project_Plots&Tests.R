74277e095269b4182c49397594e03ceb7d5f0a17
ngone8lo <59320711+ngone8lo@users.noreply.github.com>
2020-06-30 03:34:40 -0400

    Update and rename R_Code_Stats_Project_Tests.R to R_Code_Stats_Project_Plots&Tests.R

--- R_Code_Stats_Project_Plots&Tests.R
+++ R_Code_Stats_Project_Plots&Tests.R
@@ -118,3 +118,33 @@ Age_Group_factor <- as.factor(SRMAIN$Age_Group)
 Age_releveled <- relevel(Age_Group_factor, ref="55+ years")
 model_main_2 <- lm(Suicide_per_100k ~ Gender_releveled + Age_releveled, data= SRMAIN)
 summary(model_main_2)
+
+
+
+
+
+#linear regression
+library(magrittr)
+library(ggpubr)
+library(ggplot2)
+
+b <- ggplot(SRHDI, aes(x = HDI, y = Suicide_per_100k))
+# Scatter plot with regression line
+b + geom_point(shape=18) + geom_smooth(method = "lm") 
+
+
+
+# Add regression line and confidence interval
+# Add correlation coefficient: stat_cor()
+ggscatter(SRHDI, x = "HDI", y = "Suicide_per_100k",shape = 3,
+          size = 1, color = "red",
+          add = "reg.line", conf.int = TRUE,    
+          add.params = list(fill = "gray"),
+          ggtheme = theme_minimal()
+)+
+  stat_cor(
+    aes(label = paste(..rr.label.., ..p.label.., sep = "~`,`~")), 
+    label.x = 0.6, label.y=30
+  )
+
+
